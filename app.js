fbfe88e86f06759862cb5888478e61d1407f384c
haile1999 <haile3107@gmail.com>
2020-08-13 09:23:08 +0700

    set up app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+//Express instance
+const app = express();
+const PORT = process.env.PORT | 3000;
+
+app.use(function(err, req, res, next) {
+    console.log(err);
+    res.render('errors');
+});
+
+//Listen at PORT
+app.listen(PORT, () => {
+    console.log(`Listening Port: ${PORT}`);
+});
