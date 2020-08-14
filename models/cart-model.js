5a633497931a1a47861d33ca1b6ae7128cad0fba
haile1999 <haile3107@gmail.com>
2020-08-14 21:52:16 +0700

    edit models for cart manager

--- models/cart-model.js
+++ models/cart-model.js
@@ -1,10 +1,10 @@
 const db=require('../utils/db');
 module.exports={
     bookInCart:(id)=>{
-        `select * from cart c, book_cart bc, books b
+        let data=`from cart c, book_cart bc, books b
         where c.cart_id=${id} and c.cartid=bc.cart_id and bc.book_id=b.book_id`
     },
-    price:(id)=>{
+    sumPrice:(id)=>{
         `select sum(book_price) from cart c, book_cart bc, books b
         where c.cart_id=${id} and c.cartid=bc.cart_id and bc.book_id=b.book_id`
     }
