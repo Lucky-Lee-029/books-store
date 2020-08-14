542ebe646509b6aedab01341293153b4f6b337e6
tranthituyetchung <37837867+tranthituyetchung@users.noreply.github.com>
2020-08-14 16:03:28 +0700

    Add view for item page

---
+++ views/components/ItemPage/index.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './styles.sass';
+import PRODUCTS from '../Data/index';
+import Item from '../Item/index';
+
+class ItemPage extends Component {
+  product = PRODUCTS.filter((item) => item.id == this.props.match.params.id)[0];
+  componentDidMount() {
+    document.body.scrollTop = 0;
+    document.querySelector('.menu').classList.remove('open')
+    if(!this.product) this.props.history.push('/error')
+  }
+  render() {
+    return (
+      <>
+      <div className="itemPageWrapper">
+        <div className="itemImgWrapper">
+          <img src = {this.product.img}/>
+        </div>
+        <div className="itemInfoWrapper">
+          <Link className="backLink" to="/">
+            <span className="small">
+              <svg fill="#000000" height="13" viewBox="0 0 18 15" width="13" xmlns="http://www.w3.org/2000/svg">
+                <path d="M7 10l5 5 5-5z"/>
+                <path d="M0 0h24v24H0z" fill="none"/>
+              </svg>
+            </span>All Items
+          </Link>
+          <h3 className="itemName">{this.product.name}</h3>
+          <p className="itemCost frm">{this.product.price}</p>
+          <p className="description">
+            {this.product.description}
+          </p>
+          <p className="seller frm">Category: <span>{this.product.category}</span></p>
+          <button className="reqTradeBtn normalBtn">Add to cart</button>
+        </div>
+      </div>
+      <h5>You might also like</h5>
+      <div className="similar-products">
+        {PRODUCTS.map((product) => {
+          if (
+            product.category === this.product.category
+            && product.name !== this.product.name) {
+            return <Item key = {product.id} product = {product} />
+          }
+        })}
+      </div>
+      </>
+    );
+  }
+}
+
+export default ItemPage;
--- views/router/routes.js
+++ views/router/routes.js
@@ -3,12 +3,14 @@ import {BrowserRouter, Route, Switch } from 'react-router-dom';
 import App from '../components/App/index';
 import Main from '../components/Main/index';
 import ErrorPage from '../components/ErrorPage/index';
+import ItemPage from '../components/ItemPage/index';
 
 export default (
   <BrowserRouter>
     <App>
       <Switch>
         <Route path = '/' component = {Main} exact/>
+        <Route path="/item/:id" component={ItemPage} />
         <Route component={ErrorPage} />
       </Switch>
     </App>
