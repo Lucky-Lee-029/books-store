ad1f939b175b80a8f5279b5e2b477209e6db6c27
haile1999 <haile3107@gmail.com>
2020-08-15 20:19:21 +0700

    fix bug

--- app.js
+++ app.js
@@ -2,12 +2,25 @@
 var express = require('express');
 const app = express();
 const PORT = process.env.PORT | 3000;
-
+const book=require("./models/books-model");
+const pro=require("./models/profile-model");
+const account=require("./models/account-model");
+const multer=require('multer');
 app.use(function(err, req, res, next) {
     console.log(err);
     res.render('errors');
 });
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
+app.use(express.json());
+//User route
+require('./middlewares/route.mdw')(app);
+
 //Listen at PORT
-app.listen(PORT, () => {
+app.listen(PORT, async() => {
+
     console.log(`Listening Port: ${PORT}`);
 });
