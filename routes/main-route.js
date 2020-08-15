5ee7c3cd8f28ab183e5ce1d5cd6aaaac73f2f57b
haile1999 <haile3107@gmail.com>
2020-08-15 14:29:23 +0700

    route for check account

--- routes/main-route.js
+++ routes/main-route.js
@@ -1,6 +1,7 @@
 const book=require("../models/books-model");
 const cart=require("../models/cart-model");
 const profile=require("../models/profile-model");
+const account=require("../models/account-model");
 const router = require('express').Router();
 const bodyParser = require('body-parser');
 router.use(
@@ -8,25 +9,30 @@ router.use(
         extended: true
     })
 );
-router.get("/basicinfor/:id",(res,req)=>{
+router.get("/api/basicinfor/:id",(res,req)=>{
     console.log(" im here");
     console.log(req.params);
     var id = req.params.id;
     console.log(i);
     // res.send(profile.BasicInfor(id));
 });
-router.get("/ortherinfor",(res,req)=>{
+router.get("/api/ortherinfor",(res,req)=>{
     let id=req.body.id;
-    res.json(profile.OtherInfor(id));
+    res.send(profile.OtherInfor(id));
 });
 
-router.post("/books",(res,req)=>{
+router.post("/api/books",(res,req)=>{
     let id=req.body.id;
-    res.json(book.allBookInfor());
+    res.send(book.allBookInfor());
 });
 
-router.post("/search",(res,req)=>{
+router.post("/api/search",(res,req)=>{
     let name=req.body.name;
-    res.json(book.searchByName(name));
+    res.send(book.searchByName(name));
 });
+router.post("/api/auth",(res,req)=>{
+    let username=req.body.username;
+    let password=req.body.password;
+    res.send(account.checkAcount(username,password));
+})
 module.exports = router ;
